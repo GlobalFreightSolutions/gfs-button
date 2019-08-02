a95a80a4c5d86be3b16537c4069e1376b4e5b27b
Minas Pachnis <minas.pachnis@justshoutgfs.com>
2019-08-02 11:35:33 +0300

    initial circleci

--- gfs-button.js
+++ gfs-button.js
@@ -1,7 +1,6 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { PaperButtonBehavior } from '@polymer/paper-behaviors/paper-button-behavior.js';
-import '@polymer/paper-button/paper-button.js';
 import '@polymer/iron-icons/iron-icons.js';
 import './gfs-button-styles.js';
 
